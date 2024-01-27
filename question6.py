d1c47afd36f751640f0212ccbebbe933e3b224f1
Ganesh <71372490+gana36@users.noreply.github.com>
2024-01-27 00:13:13 -0500

    Update question6.py

--- question6.py
+++ question6.py
@@ -1,20 +1,20 @@
 def question6_1(): 
     answer = {
-        'equal_width': ['Change', 0], 
-        'equal_frequency': ['Change',0]
+        'equal_width': ['change', 0], 
+        'equal_frequency': ['change',0]
     }
     return answer
 
 def question6_2():
     answer = {
-        'equal_width': ['Change', 2], # doubt with 5th point
-        'equal_frequency': ['Change',2]
+        'equal_width': ['change', 2], # doubt with 5th point
+        'equal_frequency': ['change',2]
     }
     return answer
 
 def question6_3():
     answer = {
-        'equal_width': ['Change', 2], # doubt with 5th point
-        'equal_frequency': ['Change',2]
+        'equal_width': ['change', 2], # doubt with 5th point
+        'equal_frequency': ['change',2]
     }
     return answer
