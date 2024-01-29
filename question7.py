ffaa31774d9b33bd4b63e05b671ecd707ff3b2a3
Ganesh <71372490+gana36@users.noreply.github.com>
2024-01-28 19:54:51 -0500

    Update question7.py

--- question7.py
+++ question7.py
@@ -7,7 +7,7 @@ def question7_2():
     return answer
 
 def question7_3():
-    answer = [(float('-inf'), 2), (2, 3), (3, 5), (5, float('inf'))]
+    answer = [(float('-inf'), float(2)), ((float(2), float(3)), (float(3), float(5)), (float(5), float('inf'))]
     # [(-infinity, 2), [2, 3), [3, 5), [5, infinity)]
     return answer
 
