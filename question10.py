0f068345b2816216405251dae9ec59fadeff372a
Ganesh <71372490+gana36@users.noreply.github.com>
2024-01-29 20:47:55 -0500

    Update question10.py

--- question10.py
+++ question10.py
@@ -1,39 +1,53 @@
 def question10_1():
     answer=True #a
+    # It doesn't add anything if we deal with the count data
     return answer
 
 def question10_2():
     answer=True #b
+    # The correlation value between 2 real values vary between -1(strong -ve correlation) and 1 (strong +ve correlation)
     return answer
 
 def question10_3():
-    answer=True #c #check out this
+    answer=False #c
+    # Sampling might lead to information loss which is important in the case of time series. 
     return answer
 
 def question10_4():
     answer=False #d
+    # Certainly not all the outliers need not to be noise.
     return answer
 
 def question10_5():
-    answer=True #e
+    answer=False #e
+    # In a high level, noise is very inappropriate data, whereas outliers are those points that 
+    # deviate from the original data trend (pattern)
     return answer
 
 def question10_6():
-    answer=False  # check out this
-    return answer #f
+    answer=False  # f
+    # the binary atttributes are not asymmetric.
+    return answer 
 
 def question10_7():
-    answer=True
-    return answer #g
+    answer=True # g
+    # well if cosine measure is 1, that indicates the 2 vectors are parallel to each other, which means
+    # both are identical to each other.
+    return answer 
 
 def question10_8():
-    answer=True
-    return answer   #h
+    answer=False # h
+    # Its not always true, there are cases for example, if a person own 10(but not 10.5) cars, 
+    #it can be discrete and ratio (can own 0 cars)
+    return answer  
 
 def question10_9():
-    answer=False  # check out this
-    return answer   #i
+    answer=False  # i
+    # Yes quantitative variables talk about the quantity, but not necessarily the quantity needs to be continuous.
+    # cases like 10 kg, 100 meters often come under discrete, but not continuous.
+    return answer   
 
 def question10_10():
-    answer=False
-    return answer  #j
+    answer=False # j
+    # not necessarily, because we will lose the order associated between the values. 
+    return answer  
