a31d112ea7d7e272d611fdac53371c546de08b61
Ganesh <71372490+gana36@users.noreply.github.com>
2024-01-29 22:27:07 -0500

    Update question5.py

--- question5.py
+++ question5.py
@@ -17,9 +17,10 @@ def question5_2():
     return answer
 
 def question5_3():
+    # 9 - 28 , 28 - 47, 47 - 19 
     answer = {
-            'bin1': [1,2,3],
-            'bin2': [4,5,6],
-            'bin3': [7,8,9]
+            'bin1': [1,2,3,4], 
+            'bin2': [5,6,7],
+            'bin3': [8,9]
     }  
     return answer 
